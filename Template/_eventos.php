a0a1dea046319cd902849a8b721e89c4f5675f07
ymisshie <ymisshie@gmail.com>
2022-02-02 14:08:03 -0800

    Update _eventos.php

--- Template/_eventos.php
+++ Template/_eventos.php
@@ -1,14 +1,14 @@
 <!--eventos-->
 <section id="eventos">
+
     <div class="container-fluid">
 
-        <div class="row eventos-chart color-black-bg">
+        <div class="row p-md-5 eventos-chart color-black-bg">
 
             <div class="col-6 evento1 align-self-center text-white">
                 <div class="col-4 mx-auto">
                     <h5 class="etiqueta-evento p-lg-2 text-white text-center">Evento destacado</h5>
                 </div>
-
                 <div class="col-6 mx-auto text-center">
                     <h1 class="section-subtitle py-lg-3">Evento</h1>
                     <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus,
@@ -18,22 +18,21 @@
                 </div>
             </div>
 
-            <div class="col-6 evento1 align-self-center">
+            <div class="col-6 evento1 align-self-center text-white">
                 <div class="col-4 mx-auto">
-                    <h5 class="etiqueta-evento p-lg-2 text-center color-white-bg">Próximo evento</h5>
+                    <h5 class="etiqueta-evento color-white-bg  color-black p-lg-2 text-white text-center">Evento destacado</h5>
                 </div>
-
-                <div class="col-6 mx-auto text-center text-white">
+                <div class="col-6 mx-auto text-center">
                     <h1 class="section-subtitle py-lg-3">Evento</h1>
                     <p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus,
                         enim.
                     </p>
-                    <a class="btn btn-link text-white" href="#" role="button">Ver productos</a>
+                    <a class="btn btn-primary" href="#" role="button">Ver productos</a>
                 </div>
             </div>
+            
         </div>
 
     </div>
-    </div>
+
 </section>
-<!--!eventos-->
