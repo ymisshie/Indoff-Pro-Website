1c7a51c813edaedaf06285391f8ef8015c08a028
ymisshie <ymisshie@gmail.com>
2022-02-14 10:56:15 -0800

    eventos navbar

--- header.php
+++ header.php
@@ -114,7 +114,7 @@
                                     for ($x = 0; $x < $cantidad; $x++) {
                                         $item = $info_evento[$x];
                                 ?>
-                                        <li><a class="dropdown-item" href="eventos.php?id=<?php print $item['id'] ?>"><?php print $item['nombre'] ?></a></li>
+                                        <li><a class="dropdown-item" href="productos-eventos.php?id=<?php print $item['id'] ?>"><?php print $item['nombre'] ?></a></li>
 
                                 <?php
                                     }
@@ -122,26 +122,6 @@
 
                             </ul>
 
-
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <?php
-                                require 'vendor/autoload.php';
-                                $evento = new ameri\Evento;
-                                $info_evento = $evento->mostrar();
-                                $cantidad = count($info_evento);
-
-                                if ($cantidad > 0) {
-                                    for ($x = 0; $x < $cantidad; $x++) {
-                                        $item = $info_evento[$x];
-
-                                ?>
-                                        <li><a class="dropdown-item" href="eventos.php?id=<?php print $item['id'] ?>"><?php print $item['nombre'] ?></a></li>
-
-                                <?php
-                                    }
-                                } ?>
-
-                            </ul>
                         <li class="nav-item px-md-4 uppercase">
                             <a class="nav-link text-white" aria-current="page" href="#">Contacto</a>
                         </li>
